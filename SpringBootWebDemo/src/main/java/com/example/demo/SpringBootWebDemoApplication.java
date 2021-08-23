b75860d54c7f9832141a2b7b8f3e1301ce465d0c
Administrator <Administrator@host.docker.internal>
2021-08-23 11:13:30 +0530

    Sample Commit

--- SpringBootWebDemo/src/main/java/com/example/demo/SpringBootWebDemoApplication.java
+++ SpringBootWebDemo/src/main/java/com/example/demo/SpringBootWebDemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Hello
+//Hello World
 @SpringBootApplication
 public class SpringBootWebDemoApplication {
 
